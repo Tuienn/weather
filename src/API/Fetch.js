ac3bbbd1a51e9e673f2b59b9f2a91250db61370f
Tuienn <tuyenboy1234@gmail.com>
2024-08-01 10:03:53 +0700

    Completely

--- src/API/Fetch.js
+++ src/API/Fetch.js
@@ -2,7 +2,7 @@
 const GEO_API_OPTIONS = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'd6fe19888dmshaa9707258f13f81p112c45jsn2520a9b4dc94',
+      'X-RapidAPI-Key': 'rapidAPI',
       'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
     },
   };
@@ -18,7 +18,7 @@ const fetchCityData = (cityName, callback) => {
 
 //Viết call API cho Content
 const FORECAST_url = 'https://api.openweathermap.org/data/2.5'
-const FORECAST_key = '5387d63d2fb8c76c1224f21f02dcf4a7'
+const FORECAST_key = 'API key'
 const fetchForecastData = (lat, lon, callback) => {
     fetch(`${FORECAST_url}/forecast?lat=${lat}&lon=${lon}&appid=${FORECAST_key}&units=metric`)
         .then(res => res.json())
